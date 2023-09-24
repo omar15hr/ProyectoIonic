951789978f1598f67f3e1f738712cfb0836930ec
Juan Sepulveda <jp.sepulveda@duocuc.cl>
2023-09-24 20:41:16 -0300

    Agregue mis datos de Firebase y arregle lo de la imagen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,6 +15,19 @@ export const environment = {
   }
 };
 
+/*export const environment = {
+  production: false,
+  // Configuracion de firebase de JUAN
+  firebaseConfig : {
+    apiKey: "AIzaSyBwswjMwat3iBRymVmm81yQwio8-GKtNEI",
+    authDomain: "pgy4121-001d-sep.firebaseapp.com",
+    projectId: "pgy4121-001d-sep",
+    storageBucket: "pgy4121-001d-sep.appspot.com",
+    messagingSenderId: "692501145529",
+    appId: "1:692501145529:web:c2e3e706bfa1a0a9f97868"
+  }
+};
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
