b12104e323694e8d4633efdcdb66faa541db14ef
Juan Sepulveda <jp.sepulveda@duocuc.cl>
2023-11-14 19:43:02 -0300

    Correcciones de fuente y color en todas las paginas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,28 +1,30 @@
-export const environment = {
-  production: true,
-  // Configuracion de firebase de OMAR
-  firebaseConfig : {
-    apiKey: "AIzaSyA98MVaoC3DalZ7dVK-VqoAsU_qvUsyyAc",
-    authDomain: "pgy4121-001d.firebaseapp.com",
-    projectId: "pgy4121-001d",
-    storageBucket: "pgy4121-001d.appspot.com",
-    messagingSenderId: "803987788519",
-    appId: "1:803987788519:web:bd5e9a2496d75f4cf60842"
-  }
-};
-
-
-
 // export const environment = {
 //   production: true,
-//   // Configuracion de firebase de JUAN
+//   // Configuracion de firebase de OMAR
 //   firebaseConfig : {
-//     apiKey: "AIzaSyBwswjMwat3iBRymVmm81yQwio8-GKtNEI",
-//     authDomain: "pgy4121-001d-sep.firebaseapp.com",
-//     projectId: "pgy4121-001d-sep",
-//     storageBucket: "pgy4121-001d-sep.appspot.com",
-//     messagingSenderId: "692501145529",
-//     appId: "1:692501145529:web:c2e3e706bfa1a0a9f97868"
-//   }
+//     apiKey: "AIzaSyA98MVaoC3DalZ7dVK-VqoAsU_qvUsyyAc",
+//     authDomain: "pgy4121-001d.firebaseapp.com",
+//     projectId: "pgy4121-001d",
+//     storageBucket: "pgy4121-001d.appspot.com",
+//     messagingSenderId: "803987788519",
+//     appId: "1:803987788519:web:bd5e9a2496d75f4cf60842"
+//   },
+//   apiUrl:"https://dev.matiivilla.cl/duoc/location/"
 // };
+
+
+
+export const environment = {
+  production: true,
+  // Configuracion de firebase de JUAN
+  firebaseConfig : {
+    apiKey: "AIzaSyBwswjMwat3iBRymVmm81yQwio8-GKtNEI",
+    authDomain: "pgy4121-001d-sep.firebaseapp.com",
+    projectId: "pgy4121-001d-sep",
+    storageBucket: "pgy4121-001d-sep.appspot.com",
+    messagingSenderId: "692501145529",
+    appId: "1:692501145529:web:c2e3e706bfa1a0a9f97868"
+  },
+  apiUrl:"https://dev.matiivilla.cl/duoc/location/"
+};
 
